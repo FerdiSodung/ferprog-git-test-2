4b54cf2161efd0d7982857eb144a3fbbcaa37775
Ferdi Sodung <dweb9276@gmail.com>
2024-05-13 09:39:43 +0700

    menambahkan list di dalam file index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure voluptates delectus nisi quas veniam deserunt possimus, neque dolorum, quae quisquam minus, sint eveniet? Ex vel reiciendis quibusdam eaque eos laboriosam?   </p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
